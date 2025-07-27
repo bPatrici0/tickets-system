b3a7d6bdc652701b1f83b243006aa306c3883e78
bPatrici0 <b.patricio.ing@gmail.com>
2025-07-27 16:01:37 -0600

    ModificaciÂóde SecurityConfig

--- backend/Ticket/src/main/java/com/ticket/config/SecurityConfig.java
+++ backend/Ticket/src/main/java/com/ticket/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/tickets/**").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/api/tickets/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/tickets/**").hasAnyRole("ADMIN", "USER")
                 )
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);

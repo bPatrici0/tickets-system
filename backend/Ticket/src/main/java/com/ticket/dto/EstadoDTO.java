da89bb4e10024863abb695ce219e4cd92fd009ef
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-05 22:20:51 -0600

    Creacion de la clase EstadoDTO

---
+++ backend/Ticket/src/main/java/com/ticket/dto/EstadoDTO.java
@@ -0,0 +1,13 @@
+package com.ticket.dto;
+
+public class EstadoDTO {
+    private String estado;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}

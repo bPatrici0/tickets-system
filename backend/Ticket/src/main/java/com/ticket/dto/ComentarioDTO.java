d4a0d5ac5b274e9f152c4e676401e4eb93c18aac
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-05 22:16:42 -0600

    Creacion de la clase ComentarioDTO

---
+++ backend/Ticket/src/main/java/com/ticket/dto/ComentarioDTO.java
@@ -0,0 +1,22 @@
+package com.ticket.dto;
+
+public class ComentarioDTO {
+    private String contenido;
+    private String autor;
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}

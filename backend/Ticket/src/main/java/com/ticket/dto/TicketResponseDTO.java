32892dd6d0098d75101aa2a43012f4ba4808105e
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-08 21:08:54 -0600

    Creacion de getters y setters

--- backend/Ticket/src/main/java/com/ticket/dto/TicketResponseDTO.java
+++ backend/Ticket/src/main/java/com/ticket/dto/TicketResponseDTO.java
@@ -13,4 +13,52 @@ public class TicketResponseDTO {
     private EstadoTicket estado;
     private String creadoPor; // Email del usuario
     private LocalDateTime fechaCreacion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public EstadoTicket getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoTicket estado) {
+        this.estado = estado;
+    }
+
+    public String getCreadoPor() {
+        return creadoPor;
+    }
+
+    public void setCreadoPor(String creadoPor) {
+        this.creadoPor = creadoPor;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
 }

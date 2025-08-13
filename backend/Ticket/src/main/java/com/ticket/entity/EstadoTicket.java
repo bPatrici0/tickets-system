753ec6b4f3f0b5d2fa6ca31505452f226da648af
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-12 20:38:48 -0600

    Modificacion del enum

--- backend/Ticket/src/main/java/com/ticket/entity/EstadoTicket.java
+++ backend/Ticket/src/main/java/com/ticket/entity/EstadoTicket.java
@@ -1,18 +1,7 @@
 package com.ticket.entity;
 
 public enum EstadoTicket {
-    ABIERTO("ABIERTO"),
-    EN_PROGRESO("EN_PROGRESO"),
-    RESUELTO("RESUELTO");
-
-    private final String valor;
-
-    EstadoTicket(String valor) {
-        this.valor = valor;
-    }
-
-    @Override
-    public String toString() {
-        return this.valor;
-    }
+    ABIERTO,
+    EN_PROGRESO,
+    RESUELTO;
 }

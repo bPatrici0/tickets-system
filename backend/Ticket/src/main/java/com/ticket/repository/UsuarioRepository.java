71c108c30df89dbfd39b008e9fb34f551616b7db
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-23 18:24:51 -0600

    Implementacion de anotacion @Repository

--- backend/Ticket/src/main/java/com/ticket/repository/UsuarioRepository.java
+++ backend/Ticket/src/main/java/com/ticket/repository/UsuarioRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
     boolean existsByEmail(String email);

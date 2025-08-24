a18f51c1a3d2bdead5e5c887760b2c0e581c23c8
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-24 17:55:45 -0600

    Modificacion de anotacion SpringBootApplicatioon

--- backend/Ticket/src/main/java/com/ticket/TicketApplication.java
+++ backend/Ticket/src/main/java/com/ticket/TicketApplication.java
@@ -3,7 +3,7 @@ package com.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ticket")
 public class TicketApplication {
 
 	public static void main(String[] args) {

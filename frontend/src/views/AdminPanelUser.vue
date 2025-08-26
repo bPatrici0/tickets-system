58fc296740743568fbd96f38b63bb583da7906dc
bPatrici0 <b.patricio.ing@gmail.com>
2025-08-26 10:46:01 -0600

    Creacion de la vista AdminPanelUser

---
+++ frontend/src/views/AdminPanelUser.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="min-h-screen bg-black p-4">
+        <header class="terminal-box mb-6">
+            <div class="flex justify-between items-center">
+                <h1 class="text-2xl">> Gestion de Usuarios Panel<span>|</span></h1>
+                <div>
+                    <button @click="$router.push('/admin')" class="btn-matrix text-sm mr-2">
+                        > Volver al panel
+                    </button>
+                    <button>
+                        > Cerrar Sesión
+                    </button>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>

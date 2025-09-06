75a385e3e849a9e77927e3e45a2ee040928ae117
bPatrici0 <b.patricio.ing@gmail.com>
2025-09-06 11:12:00 -0600

    Creacion de la clase UsuarioUpdateDTO.java

---
+++ backend/Ticket/src/main/java/com/ticket/dto/UsuarioUpdateDTO.java
@@ -0,0 +1,40 @@
+package com.ticket.dto;
+
+public class UsuarioUpdateDTO {
+    private String nombre;
+    private String email;
+    private String rol;
+    private Boolean activo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}

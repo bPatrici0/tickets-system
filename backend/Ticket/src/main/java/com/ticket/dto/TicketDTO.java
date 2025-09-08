780737103b4041c4fd7da0e9a3a262a91bf78f5d
bPatrici0 <b.patricio.ing@gmail.com>
2025-09-07 22:17:58 -0600

    Creacion de getters y setters

--- backend/Ticket/src/main/java/com/ticket/dto/TicketDTO.java
+++ backend/Ticket/src/main/java/com/ticket/dto/TicketDTO.java
@@ -9,4 +9,28 @@ public class TicketDTO {
     private String titulo;
     private String descripcion;
     private String estado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }

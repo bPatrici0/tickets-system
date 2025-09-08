ad8851f8db562ee3bf1de4898bd13be12fdfa86d
bPatrici0 <b.patricio.ing@gmail.com>
2025-09-08 12:18:08 -0600

    Creacion de la nueva vista AdminTicketEstatus.vue

---
+++ frontend/src/views/AdminTicketEstatus.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-screen bg-black p-4">
+        <header class="terminal-box mb-6">
+            <div class="flex justify-between items-center">
+                <h1 class="text-2xl">
+                    > Administracion - Ticket #{{ ticket.id }}<span class="cursor-blink">|</span>
+                </h1>
+            </div>
+        </header>
+    </div>
+</template>

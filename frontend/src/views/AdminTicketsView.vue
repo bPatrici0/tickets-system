99d53ac9114f37db28e6a427d2732c6ba205f430
bPatrici0 <b.patricio.ing@gmail.com>
2025-09-13 13:14:14 -0600

    Modificaiones menores

--- frontend/src/views/AdminTicketsView.vue
+++ frontend/src/views/AdminTicketsView.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen bg-black p-4">
     <header class="terminal-box mb-6">
       <div class="flex justify-between items-center">
-        <h1 class="text-2xl">> Administración - Todos los Tickets<span class="cursor-blink">|</span></h1>
+        <h1 class="text-2xl">> Admin - Todos los Tickets<span class="cursor-blink">|</span></h1>
         <div>
           <button @click="$router.push('/admin')" class="btn-matrix text-sm mr-2">
             > Volver al Panel

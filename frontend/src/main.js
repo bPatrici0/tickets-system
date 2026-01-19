b898b59b4ba68dbbb9bac94fbab69f2f1dc32948
bPatrici0 <b.patricio.ing@gmail.com>
2026-01-19 17:49:52 -0600

    Correcion en las notificaciones

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.css'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App)
     .use(router)

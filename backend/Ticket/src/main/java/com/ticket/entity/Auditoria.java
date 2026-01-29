30f340079ff8696ef82f8ac7815ee39ca753718e
bPatrici0 <b.patricio.ing@gmail.com>
2026-01-29 11:49:37 -0600

    Creacion de getters y setters

--- backend/Ticket/src/main/java/com/ticket/entity/Auditoria.java
+++ backend/Ticket/src/main/java/com/ticket/entity/Auditoria.java
@@ -17,4 +17,67 @@ public class Auditoria {
     private String detalles;
     private LocalDateTime fecha = LocalDateTime.now();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+
+    public String getValorAnterior() {
+        return valorAnterior;
+    }
+
+    public void setValorAnterior(String valorAnterior) {
+        this.valorAnterior = valorAnterior;
+    }
+
+    public String getValorNuevo() {
+        return valorNuevo;
+    }
+
+    public void setValorNuevo(String valorNuevo) {
+        this.valorNuevo = valorNuevo;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
 }

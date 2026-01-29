b4fa14e4489f7eae25b1bdc53b2a7f1ad47d2c1d
bPatrici0 <b.patricio.ing@gmail.com>
2026-01-29 12:03:42 -0600

    Creacion de la clase AuditoriaService.java

---
+++ backend/Ticket/src/main/java/com/ticket/service/AuditoriaService.java
@@ -0,0 +1,14 @@
+package com.ticket.service;
+
+import com.ticket.entity.Auditoria;
+import com.ticket.repository.AuditoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+
+public class AuditoriaService {
+
+}

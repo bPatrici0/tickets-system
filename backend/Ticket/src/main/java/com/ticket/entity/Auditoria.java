585484ea8671ed21a417927b478624253541fb78
bPatrici0 <b.patricio.ing@gmail.com>
2026-01-28 23:18:10 -0600

    Creacion de nueva entidad Auditoria.java

---
+++ backend/Ticket/src/main/java/com/ticket/entity/Auditoria.java
@@ -0,0 +1,11 @@
+package com.ticket.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Auditoria {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+}

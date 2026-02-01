25fb72f132b740505a5be3da59961e824c4edb82
bPatrici0 <b.patricio.ing@gmail.com>
2026-01-31 19:07:33 -0600

    Getters y Setters

--- backend/Ticket/src/main/java/com/ticket/dto/PerformanceDTO.java
+++ backend/Ticket/src/main/java/com/ticket/dto/PerformanceDTO.java
@@ -8,4 +8,20 @@ public class PerformanceDTO {
         this.usuario = usuario;
         this.cantidadResoluciones = cantidadResoluciones;
     }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Long getCantidadResoluciones() {
+        return cantidadResoluciones;
+    }
+
+    public void setCantidadResoluciones(Long cantidadResoluciones) {
+        this.cantidadResoluciones = cantidadResoluciones;
+    }
 }
